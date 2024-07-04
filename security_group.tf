7c520799c1c6e8ad5fe9185b4ed7e376a788224f
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-05 00:30:35 +0500

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -46,3 +46,27 @@ resource "aws_security_group" "webserver" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+#=================================================================================#
+
+Переменные в файле variables.tf
+
+resource "aws_security_group" "web_server" {
+  name   = "My Security Group"
+  dynamic "ingress" {
+    for_each = var.allow_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.common_tags, { Name = "${var.common_tags["Environment"]} Server SecurityGroup" })     # сливаем/суммируем(merge) common_tags из variables.tf + новую переменную Name
+}
