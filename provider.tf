951797e4dcc2ab6064beda7422d04f6a5601aad7
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 21:41:00 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -20,3 +20,28 @@ provider "aws" {
 }
 
 #=================================================================================# 
+
+Использование другого AWS аккаунта
+
+provider "aws" {
+  alias      = "ANOTHER_AWS_ACCOUNT"
+  region     = "ca-central-1"
+  access_key = "xxxxxxxxxxxx"
+  secret_key = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::1234567890:role/RemoteAdministrators"
+    session_name = "TERRAFROM_SESSION"
+  }
+}
+#=================================================================================# 
+
+Если есть несколько провайдеров и сервов,чтобы серв обращался к определенному провайдеру по его alias
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "USA"
+}
+...
+instance : provider    = aws.USA
+...
