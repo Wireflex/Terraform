2763efcadda16ce043fa241b6d8c6c0d333c5be0
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 22:23:57 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,24 @@ variable "region" {
   default     = "eu-central-1"                              # Если нужно изменить default при запуске,тогда в строке terraform apply -var="region=us-east-2"
 }
 
+variable "instance_type" {
+  description = "Enter Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "allow_ports" {
+  description = "List of Ports to open for server"
+  type        = list
+  default     = ["80", "443", "22"]
+}
+
+variable "common_tags" {
+  description = "Common Tags to apply to all resources"
+  type        = map
+  default = {
+    Owner       = "Wireflex"
+    CostCenter  = "12345"
+    Environment = "development"
+  }
+}
