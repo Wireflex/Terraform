1110073d02d3afbab34fdbca0587bd3f3b08e23b
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 22:02:06 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "region" {
   description = "Please Enter AWS Region to deploy Server"
   type        = string
-  default     = "eu-central-1"
+  default     = "eu-central-1"                              # Если нужно изменить default при запуске,тогда в строке terraform apply -var="region=us-east-2"
 }
+
