e2cb021aa3fad527887def5517a41b6bdc5ce6fb
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 22:41:39 +0500

    Create template.tf

---
+++ template.tf
@@ -0,0 +1,19 @@
+Эта штука нужна для заполнения index.html в файле user_data.sh.tpl( который в репозитории user_data.sh )
+resource "aws_instance" "webserver" {
+  ami           = "ami-0a3041ff14fb6e2be"
+  instance_type = "t2.micro"
+  key_name      = "private-ssh-key"
+
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Nikita",
+    l_name = "Utyaganov",
+    names  = ["Vasya", "Kolya", "Petya", "John", "Donald", "Masha"]
+  })
+
+  tags = {
+    Name  = "Web Server Build by Terraform"
+    Owner = "Wireflex"
+  }
+}
