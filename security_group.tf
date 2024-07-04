c0cc33946db1f88fd9ba9ae70ace7c1c2fed6825
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 23:06:21 +0500

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,48 @@
+Перечисление каждого tcp
+
+resource "aws_security_group" "webserver" {
+  name = "webserver group"
+  description = "Security group for webserver instance"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#=================================================================================#
+
+Динамический блок
+
+  dynamic "ingress" {
+    for_each = ["22", "80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
