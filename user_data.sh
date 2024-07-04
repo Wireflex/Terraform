3c03ae75f94b2f34894b24017837aecb36727ae9
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-04 22:27:45 +0500

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+cat <<EOF > /var/www/html/index.html
+<html>
+<h2>Build by Power of Terraform <font color="red"> v0.12</font></h2><br>
+</html>
+EOF
+systemctl start httpd
+systemctl enable httpd
+
+#=================================================================================# 
+
+Если добавить templates,то файл переименовываем в user_data.sh.tpl
+
+#!/bin/bash
+yum update -y
+yum install httpd -y
+cat <<EOF > /var/www/html/index.html
+<html>
+<h2>Build by Power of Terraform <font color="red"> v0.12</font></h2><br>
+Owner ${f_name} ${l_name} <br>
+
+%{ for x in names ~}
+Hello to ${x} from ${f_name}<br>
+%{ endfor ~}
+
+</html>
+EOF
+
+systemctl start httpd
+systemctl enable httpd
