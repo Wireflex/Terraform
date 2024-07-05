7e4dc5a8cdbd22e8aa0e58e9ba902a2db0a6c20d
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-05 23:52:44 +0500

    Update variables.tf

--- General_Version/variables.tf
+++ General_Version/variables.tf
@@ -1,1 +1,26 @@
+variable "region" {
+  description = "Please Enter AWS Region to deploy Server"
+  type        = string
+  default     = "eu-central-1"
+}
 
+variable "allow_ports" {
+  description = "List of Ports to open for server"
+  type        = list
+  default     = ["22", "80", "443"]
+}
+
+variable "instance_type" {
+  description = "Enter Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "common_tags" {
+  description = "Common Tags to apply to all resources"
+  type        = map
+  default = {
+    Owner       = "Wireflex"
+    Environment = "development"
+  }
+}
