fb20b8dc2bf5094c1e8d367759bed2e1f269f0cd
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-07-05 23:53:05 +0500

    Update user_data.sh

--- General_Version/user_data.sh
+++ General_Version/user_data.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+cat <<EOF > /var/www/html/index.html
+<html>
+<h2>Final version of Terraform lesson <font color="red"> 05.07.2024</font></h2><br>
+</html>
+EOF
+systemctl start apache2
+systemctl enable apache2
