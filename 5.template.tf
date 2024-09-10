2109e31d284798f530fb8f7187c2275d4e821edb
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-09-11 00:25:46 +0500

    Update and rename template.tf to 5.template.tf

--- 5.template.tf
+++ 5.template.tf
@@ -18,3 +18,24 @@ resource "aws_instance" "webserver" {
     Owner = "Wireflex"
   }
 }
+
+
+user_data.sh.tpl тогда будет таким
+
+#!/bin/bash
+yum update -y
+yum install httpd -y
+cat <<EOF > /var/www/html/index.html
+<html>
+<h2>Build by Power of Terraform <font color="red"> v0.12</font></h2><br>
+Owner ${f_name} ${l_name} <br>
+
+%{ for x in names ~}
+Hello to ${x} from ${f_name}<br>
+%{ endfor ~}
+
+</html>
+EOF
+
+systemctl start httpd
+systemctl enable httpd
