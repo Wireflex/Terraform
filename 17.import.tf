b4e9171999c7bef3942656c6a937759a6d5beb4e
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-09-13 19:43:57 +0500

    Update 17.import.tf

--- 17.import.tf
+++ 17.import.tf
@@ -27,3 +27,24 @@ terraform init
 terraform import aws_security_group.main sg-1234567890abcdef0   # (SG ID)
 
 terraform import aws_instance.instance_1 i-1234567890abcdef0  # ( Instance ID)
+
+
+
+либо блоком импортить
+
+import { # Web
+  id = "sg-1234567890qwerty"
+  to = aws_security_group.web
+}
+
+import { # Sql
+  id = "sg-ytrewq0987654321"
+  to = aws_security_group.sql
+}
+
+но этих ресурсов изначально нет(у нас пустой tfstate, к примеру), терраформ сам подскажет, что нужно создать
+```terraform plan -generate-config-out-generated.tf```
+
+```terraform plan -generate-config-out-generated-sg.tf``` для СГ
+
+для всего остального так же, может писать ошибки, т.к некоторые параметры конфликтуют с другими, но пофиг на это, файлы он создаст и ок
